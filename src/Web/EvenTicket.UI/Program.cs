d5a005c8a0fde9c14b30050ad5775c1d36ee56bc
Amit P Naik <amit.naik8103@gmail.com>
2025-09-05 08:56:25 +0530

    feat: adding from baseurl

--- src/Web/EvenTicket.UI/Program.cs
+++ src/Web/EvenTicket.UI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Read base URL from config
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "https://localhost:5056";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
 await builder.Build().RunAsync();
